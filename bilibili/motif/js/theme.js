61d4939c9eec09812f17891243b246d562044eb9
Meraks <1429687705@qq.com>
2018-07-29 11:54:24 +0800

    最新的进度

--- bilibili/motif/js/theme.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var me=$("li");
-  me.mouseover(function () {
-        $(this).children("div").addClass("showme");
-        $(this).siblings().children("div").removeClass("showme");
-        }).children().mouseout(function () {
-        $(this).removeClass("showme");
-    })
-});
